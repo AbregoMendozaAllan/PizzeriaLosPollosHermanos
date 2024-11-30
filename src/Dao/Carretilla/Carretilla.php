7668b258536a1604a85200568b5d0466baa281d9
Esdras <rankeygd@gmail.com>
2024-11-29 19:59:08 -0600

    dao Carretilla

---
+++ src/Dao/Carretilla/Carretilla.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dao\Carretilla;
+
+use Dao\Table;
+
+Class Carretilla extends Table{
+
+    Public static function getCarretilla(){
+
+    }
+
+    Public static function InsertIntoCarretilla(){
+
+    }
+
+    Public static function GetDetalleCarretilla(){
+        
+    }
+
+}
